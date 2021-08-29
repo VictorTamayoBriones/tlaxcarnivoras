4ac6f6ad18ed643ba43edde2cef3259157c63c05
VictorTamayoBriones <victortamayo509@gmail.com>
2021-08-29 00:39:12 -0500

    Tlaxcarnivoras

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import favicon from './logo.png';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <Helmet>
+      <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+      <title>Tlaxcarnivoras</title>
+    </Helmet>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
