107e23a6fbc4be88716d52f26d65db56317ac4ca
VictorTamayoBriones <victortamayo509@gmail.com>
2021-10-04 12:06:03 -0500

    helmet-async

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import { Nav, Toggle } from './styled';
 import { Button } from '../Buttons';
 import { Link, useHistory } from 'react-router-dom'
-import { HelmetProvider, Helmet } from 'react-helmet-async';
 import "animate.css"
 
 const Navbar = () => {
@@ -15,29 +14,20 @@ const Navbar = () => {
     const handleClick = ({target:{name}}) => history.push(`/${name}`)
 
     return (  
-        <>
-            <HelmetProvider>
-                <Helmet>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                    <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet" /> 
-                </Helmet>
-                <Nav>
-                    <h2>TLAXCARNIVORAS</h2>
-                    <Toggle>
-                        <svg onClick={()=>setMenu(!menu)} xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                        </svg>
-                        <ul className={menu ? 'visible animate__animated animate__fadeInRight' : null}>
-                            <li><Link to="/" >Contacto</Link></li>
-                            <li><Link to="/" >Galeria</Link></li>
-                            <li><Button onClick={handleClick} as={ motion.button } whileHover={{ y: -5, x: 2}} name='login' >Log in</Button></li>
-                            <li><Button onClick={handleClick} as={ motion.button } whileHover={{ y: -5, x: 2}} secondary name='logup' >Log up</Button></li>
-                        </ul>
-                    </Toggle>
-                </Nav>
-            </HelmetProvider>
-        </>
+        <Nav>
+            <h2>TLAXCARNIVORAS</h2>
+            <Toggle>
+                <svg onClick={()=>setMenu(!menu)} xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                <ul className={menu ? 'visible animate__animated animate__fadeInRight' : null}>
+                    <li><Link to="/" >Contacto</Link></li>
+                    <li><Link to="/" >Galeria</Link></li>
+                    <li><Button onClick={handleClick} as={ motion.button } whileHover={{ y: -5, x: 2}} name='login' >Log in</Button></li>
+                    <li><Button onClick={handleClick} as={ motion.button } whileHover={{ y: -5, x: 2}} secondary name='logup' >Log up</Button></li>
+                </ul>
+            </Toggle>
+        </Nav>
     );
 }
 
